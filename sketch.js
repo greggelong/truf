60bdf17653985386ef8c76e20f01d37a96e07fe4
Greg K <gkreisman_cep@qq.com>
2021-08-12 22:55:01 +0800

    add checkboxes

--- sketch.js
+++ sketch.js
@@ -1,14 +1,22 @@
 let greg; // name of the gurtle
+let cbox;
+let kbox;
+let pbox;
+let bbox;
 
 function setup() {
   
   createCanvas(600, 600);
   background(32)
+  kbox = createCheckbox('Koch curve',true);
+  cbox = createCheckbox('cauliflower',true);
+  pbox = createCheckbox('Escher penguin (only box checked)',false);
+  bbox = createCheckbox('cauli-bug (only box checked)',false);
   angleMode(DEGREES);// need angle mode set to degrees
  // create a red gurtle named greg in the center of the canvas
   greg = new Gurtle(width/4,height/2,color(0,255,0));
   frameRate(1);
-  print(greg.clr)
+
 }
 
 function draw() {
@@ -17,34 +25,44 @@ function draw() {
   let clevel = floor(map(mouseX,0,width,0,15,true));
   let klevel = floor(map(mouseX,0,width,0,6,true));
   let plevel = floor(map(mouseX,0,width,0,9,true));
-  /*
-  // reset the color and position of the gurtle for cauliflower
-  greg.clr = color(0,255,0)
-  greg.x = width/4;
-  greg.y = height/2;
-  greg.angle =0;
-  cauliflower(greg,clevel,300);
   
+
+  if (cbox.checked()){
+  // reset the color and position of the gurtle for cauliflower
+    greg.clr = color(0,255,0)
+    greg.x = width/4;
+    greg.y = height/2;
+    greg.angle =0;
+    cauliflower(greg,clevel,300);
+  }
+
+  if (kbox.checked()){
   // reset the color and position of the gurtle for koch
-  greg.clr = color(255,200,0);
-  greg.angle =0
-  greg.x =0;
-  greg.y =height/3;
-  koch(greg,klevel,600)
+    greg.clr = color(255,200,0);
+    greg.angle =0
+    greg.x =0;
+    greg.y =height/3;
+    koch(greg,klevel,600)
+  }
 
-  greg.clr = color(255,200,0);
-  greg.angle =0
-  greg.x = width/2;
-  greg.y =height/3;
-  penguin(greg,plevel,100);
+  if (pbox.checked() && !kbox.checked() && !cbox.checked() && !bbox.checked()){
+    // position and color of penguin
+    greg.clr = color('yellow');
+    greg.angle =0
+    greg.x = width/2;
+    greg.y =height/3;
+    penguin(greg,plevel,100);
 
-  */
-  greg.clr = color(255,200,0);
-  greg.angle =0
-  greg.x = width/4;
-  greg.y =height/4;
-  caulibug(greg,clevel,200);
- 
+  }
+
+  if (bbox.checked() && !kbox.checked() && !cbox.checked() && !pbox.checked()){
+    // position and color of caulibug
+    greg.clr = color('orange');
+    greg.angle =0
+    greg.x = width/4;
+    greg.y =height/4;
+    caulibug(greg,clevel,200);
+  }
 }
 
 // koch curve
@@ -116,6 +134,5 @@ function caulibug(t,level,size){
   cauliflower(t,level,size);
   t.right(60);
   cauliflower(t,level,size);
-
 
 }
